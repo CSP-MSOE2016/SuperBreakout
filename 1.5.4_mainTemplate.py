50ebb2240cd31c2512b9d3df7a2c715baffff711
JakePo50 <jake.popanda@badger.k12.wi.us>
2016-06-10 09:38:15 -0500

    initial template

---
+++ 1.5.4_mainTemplate.py
@@ -0,0 +1,27 @@
+####
+# MSOE PLTW CSP Core Training
+# 1.5.4 Canvas Template
+####
+
+import Tkinter #often people import Tkinter as *
+
+#####
+# Create root window 
+####
+root = Tkinter.Tk()
+
+text = Tkinter.Label(root, text='Place off screen buttons here')
+text.grid(row=0, column=4)
+
+######
+# Create View
+#######
+# Create and place a canvas
+canvas = Tkinter.Canvas(root, width=800, height=600, background='#AAAAAA')
+canvas.grid(row=2, rowspan=2, column=4)
+
+
+#######
+# Event Loop
+#######
+root.mainloop()
