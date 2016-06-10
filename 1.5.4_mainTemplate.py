6a18eaf82bb8f3b0318dae78660507de02428ac7
DeannaSoukup <deanna.soukup@lsr7.net>
2016-06-10 10:33:52 -0500

    We added the ball

--- 1.5.4_mainTemplate.py
+++ 1.5.4_mainTemplate.py
@@ -20,7 +20,42 @@ text.grid(row=0, column=4)
 canvas = Tkinter.Canvas(root, width=800, height=600, background='#AAAAAA')
 canvas.grid(row=2, rowspan=2, column=4)
 
+# Create a circle on the canvas to match the initial model
+speed_intvar = Tkinter.IntVar()
+speed_intvar.set(3) # Initialize y coordinate
+# radius and x-coordinate of circle
+r = 10
+x = 150
+y = 150
+direction = 0.5 # radians of angle in standard position, ccw from positive x axis
+ 
+circle_item = canvas.create_oval(x-r, y-r, x+r, y+r, 
+                                 outline='#000000', fill='#00FFFF')
+import math
+def animate():
+    # Get the slider data and create x- and y-components of velocity
+    velocity_x = speed_intvar.get() * math.cos(direction) # adj = hyp*cos()
+    velocity_y = speed_intvar.get() * math.sin(direction) # opp = hyp*sin()
+    # Change the canvas item's coordinates
+    canvas.move(circle_item, velocity_x, velocity_y)
+    
+    # Get the new coordinates and act accordingly if ball is at an edge
+    x1, y1, x2, y2 = canvas.coords(circle_item)
+    global direction
+    # If crossing left or right of canvas
+    if x2>canvas.winfo_width() or x1<0: 
+        direction = math.pi - direction # Reverse the x-component of velocity
+    # If crossing top or bottom of canvas
+    if y2>canvas.winfo_height() or y1<0: 
+        direction = -1 * direction # Reverse the y-component of velocity
+    
+    # Create an event in 1 msec that will be handled by animate(),
+    # causing recursion        
+    canvas.after(1, animate)
+# Call function directly to start the recursion
+animate()
 
+#test
 #######
 # Event Loop
 #######
