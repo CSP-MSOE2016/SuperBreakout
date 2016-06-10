6f0998f10677ec1163245dd06f6c69948bf6fb2c
rothede <rothede@msoe.edu>
2016-06-10 12:59:59 -0500

    Ball and blocks merged

--- 1.5.4_mainTemplate.py
+++ 1.5.4_mainTemplate.py
@@ -5,6 +5,7 @@
 
 import Tkinter #often people import Tkinter as *
 import random
+import math
 ''' Test comment for syncing
 '''
 
@@ -53,7 +54,7 @@ def process_blocks(list_of_blocks,canvas,ball):
         if min(yba1, yba2) <= max(ybl1,ybl2):
             if (xba1 + xba2)/2 >= min(xbl1,xbl2) and (xba1 + xba2)/2 <= max(xbl1,xbl2):
                  # collision!!!
-                 
+                 list_of_blocks.remove(block)
                  # for now, make block black
                  canvas.itemconfig(block, fill="black")
                  return True  # there was a collision
@@ -86,7 +87,7 @@ def draw_rows(rows):
             
 draw_rows(6)    
 
-ball = canvas.create_oval(400, 300, 420, 320, outline='#000000', fill='#00FFFF')      
+'''
     
 def callback():
     xba1, yba1, xba2, yba2 = canvas.coords(ball)
@@ -97,6 +98,82 @@ b = Tkinter.Button(root, text="OK", command=callback)
 b.grid(row=0, column=1)
       
 ball = canvas.create_oval(400, 300, 420, 320, outline='#000000', fill='#00FFFF')      
+'''
+
+# Create a circle on the canvas to match the initial model
+speed_intvar = Tkinter.IntVar()
+speed_intvar.set(10) # Initialize y coordinate
+# radius and x-coordinate of circle
+r = 10
+x = 300
+y = 400
+direction = 1 # radians of angle in standard position, ccw from positive x axis
+ 
+circle_item = canvas.create_oval(x-r, y-r, x+r, y+r, 
+                                 outline='#000000', fill='#00FFFF')
+
+def animate():
+    # Get the slider data and create x- and y-components of velocity
+    velocity_x = speed_intvar.get() * math.cos(direction) # adj = hyp*cos()
+    velocity_y = speed_intvar.get() * math.sin(direction) # opp = hyp*sin()
+    # Change the canvas item's coordinates
+    canvas.move(circle_item, velocity_x, velocity_y)
+    global direction
+    
+    # check for collision with bricks
+    if process_blocks(ref_list,canvas,circle_item):
+        # there was was a collision
+        # If crossing top or bottom of canvas
+        direction = -1 * direction # Reverse the y-component of velocity
+        
+
+    
+    
+    
+    
+    # Get the new coordinates and act accordingly if ball is at an edge
+    x1, y1, x2, y2 = canvas.coords(circle_item)
+    # If crossing left or right of canvas
+    if x2>canvas.winfo_width() or x1<0: 
+        direction = math.pi - direction # Reverse the x-component of velocity
+    # If crossing top or bottom
+    if y2>canvas.winfo_height(): 
+        direction = -1 * direction # Reverse the y-component of velocity
+    
+    # Create an event in 1 msec that will be handled by animate(),
+    # causing recursion        
+    canvas.after(1, animate)
+# Call function directly to start the recursion
+animate()
+
+#test
+#######
+# Event Loop
+#######
+root.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #######
 # Event Loop
