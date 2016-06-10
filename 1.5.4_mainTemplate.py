1d4ef9533bbbefd8d6e2a17444e3e9f1ef64aecf
rothede <rothede@msoe.edu>
2016-06-10 11:02:05 -0500

    Collision detection

--- 1.5.4_mainTemplate.py
+++ 1.5.4_mainTemplate.py
@@ -38,8 +38,26 @@ text.grid(row=0, column=4)
 ######
 '''counter = 0
 while counter < 5:
-'''    
-canvas.create_rectangle(5,5,100,25, fill="red")
+'''
+blocks = [] #empty list    
+blocks.append(canvas.create_rectangle(5,5,100,25, fill="red"))
+
+def process_blocks(list_of_blocks,canvas,ball):
+    
+    for block in list_of_blocks:
+        xbl1, ybl1, xbl2, ybl2 = canvas.coords(block)
+        xba1, yba1, xba2, yba2 = canvas.coords(ball)
+        # check for collision - that is, if ball's min y <= blocks's
+        # max y (since we are upsidedown), and ball's mid x is with block's
+        # x range
+        if min(yba1, yba2) <= max(ybl1,ybl2):
+            if (xba1 + xba2)/2 >= min(xbl1,xbl2) and (xba1 + xba2)/2 <= max(xbl1,xbl2):
+                 # collision!!!
+                 
+                 # for now, make block black
+                 canvas.itemconfig(block, fill="black")
+                 return True  # there was a collision
+    return False
 
 #######
 # Event Loop
